a955cfc4b8f26f2604226cbe3fac9d3229ad43a9
Leonid <leonid.atf@gmail.com>
2019-09-30 21:02:29 +0300

    Update settings model

--- Microting.EformMonitoringBase/Infrastructure/Models/Settings/MonitoringBaseSettings.cs
+++ Microting.EformMonitoringBase/Infrastructure/Models/Settings/MonitoringBaseSettings.cs
@@ -5,5 +5,8 @@
         public string LogLevel { get; set; }
         public string LogLimit { get; set; }
         public string SdkConnectionString { get; set; }
+        public string SendGridApiKey { get; set; }
+        public string FromEmailAddress { get; set; }
+        public string FromEmailName { get; set; }
     }
 }
