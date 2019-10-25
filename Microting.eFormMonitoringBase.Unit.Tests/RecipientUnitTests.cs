fc313e9c54523eece92ff351e666d76dc6b81ca2
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-10-25 11:10:32 +0200

    adding initial unit tests for NotificationRule

---
+++ Microting.eFormMonitoringBase.Unit.Tests/RecipientUnitTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microting.EformMonitoringBase.Infrastructure.Data.Entities;
+using NUnit.Framework;
+
+namespace Microting.eFormMonitoringBase.Unit.Tests
+{
+    [TestFixture]
+    public class RecipientUnitTests : DbTestFixture
+    {
+        [Test]
+        public void Recipient_Save_DoesSave()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+        }
+    }
+}
